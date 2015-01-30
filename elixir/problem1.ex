b13f4400e510f192a2ce76861ebd95a07848a662
pottereric <eric.b.potter@gmail.com>
2015-01-30 13:20:10 -0500

    finished problem 1

--- elixir/problem1.ex
+++ elixir/problem1.ex
@@ -1,31 +1,29 @@
 # Define the module. It must start with a capital letter
 defmodule Problem1 do
-
 	def solve() do
+		# Use of the pipe forward 
 		getMultiplesOf3And5 |> sumList
 	end
 
 	def getMultiplesOf3And5 do
 		#define a list
-		# TODO - calculate the list
-		#[ 3, 5, 6, 9 ]
-		return makeList(3)
+		makeList(3)
 	end
 
-	def makeList(9)
+	def makeList(1000) do
 		[]
 	end
 
-	def makeList(num)
-		if(rem(num, 3) == 0 or rem(num,5) == 0)
+	def makeList(num) do
+		# remember to add the 'do' for the if statement
+		if(rem(num, 3) == 0 or rem(num,5) == 0) do
 			output = [num] ++ makeList(num + 1)
 		else
 			output = makeList(num + 1)
 		end		
 		
-		output
+		#output
 	end
-
 
 	# The method sumList is overloaded and resolved with pattern matching.
 	# If the method is called with a list that has a head and a tail, the 
@@ -38,6 +36,4 @@ defmodule Problem1 do
 	def sumList ([]) do
 		0
 	end
-
-	
 end
